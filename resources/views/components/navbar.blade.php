bf58f62a5107e0842b02d30db211a055817f1f18
iftekharabir <iftekhar.abir1@gmail.com>
2023-12-19 23:36:04 +0600

    Done

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav>
+        <div class="flex flex-wrap items-center justify-between bg-orange-100 md:p-5">
+            <div class="p-4 px-4 text-2xl font-bold">Shopkeeper Store</div>
+            <div class="p-4 md:hidden">
+                <img id="menu-button"
+                    class="h-6 transition-opacity duration-500 ease-in-out cursor-pointer hover:opacity-[0.5]"
+                    src="/static_files/svgs/bars-black.svg" alt="list icon">
+            </div>
+            <div id="menu" class="hidden md:block">
+                <ul class="items-center w-screen text-lg md:w-auto md:flex">
+                    <li
+                        class="p-4 py-3 md:mx-3  font-bold border-b custom md:bg-[#F8DDBE] border-orange-50 rounded md:border-0">
+                        <a href="/">Home</a>
+                    </li>
+                    <li class="p-4 border-b border-orange-50 md:px-7 md:border-0"><a href="/products">All Products</a></li>
+                    <li class="p-4 border-b border-orange-50 md:px-7 md:border-0"><a href="/products/create">Add Product</a></li>
+                    <li class="p-4 border-b border-orange-50 md:px-7 md:border-0"><a href="/sales">Sell Product</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <br>
